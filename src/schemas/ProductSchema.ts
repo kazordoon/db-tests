fe7400d49b67e0d67b705ffb4e3acf1e2e3186ef
Felipe Barros <felipetech@tuta.io>
2020-05-22 19:38:15 -0300

    chore: Add method to create a new product

--- src/schemas/ProductSchema.ts
+++ src/schemas/ProductSchema.ts
@@ -1,12 +1,23 @@
 import Knex from 'knex'
 import Crud from '../models/Crud'
+import CreateProduct from '../models/CreateProduct'
 
 class ProductSchema extends Crud {
   public constructor (protected connection: Knex) {
     super(connection)
   }
 
-  public async create (product) {}
+  /**
+   * Create a new product
+   * @param {CreateProduct} product
+   * @return Generated id
+   */
+  public async create (product: CreateProduct) {
+    const productId = await this.connection('products')
+      .insert(product)
+      .returning('id')
+    return productId
+  }
 
   public find (product) {}
 
