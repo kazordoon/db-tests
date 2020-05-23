6a312af3dd128617dad1ae00c17a7a0794b56241
Felipe Barros <felipetech@tuta.io>
2020-05-23 20:55:19 -0300

    test: Add product update test

--- __tests__/product.test.ts
+++ __tests__/product.test.ts
@@ -1,4 +1,5 @@
 import CreateProduct from '../src/models/CreateProduct'
+import UpdateProduct from '../src/models/UpdateProduct'
 import ProductSchema from '../src/schemas/ProductSchema'
 import connection from '../src/database/connection'
 
@@ -6,6 +7,9 @@ const CREATE_PRODUCT_MOCK: CreateProduct = {
   name: 'Notebook',
   description: 'An awesome notebook',
   price: 700.99
+}
+const UPDATE_PRODUCT_MOCK: UpdateProduct = {
+  price: 1699.50
 }
 
 describe('Product', () => {
@@ -33,5 +37,16 @@ describe('Product', () => {
 
     expect(products).toBeDefined()
     expect(products[0].id).toBe(productId)
+  })
+
+  it('should update a product', async () => {
+    const productSchema = new ProductSchema(connection)
+
+    const productId = await productSchema.create(CREATE_PRODUCT_MOCK)
+
+    const product = await productSchema.update(productId, UPDATE_PRODUCT_MOCK)
+
+    expect(product).toBeDefined()
+    expect(product.price).toBe(UPDATE_PRODUCT_MOCK.price)
   })
 })
