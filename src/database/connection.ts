7972f22c07f35e15270eb2dc9d21a610a03f628d
Felipe Barros <felipetech@tuta.io>
2020-05-23 20:53:11 -0300

    fix: Set the postgres numeric type as a number

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,6 +1,13 @@
 import knex from 'knex'
+import pg, { types } from 'pg'
 
 import config from '../../knexfile'
+
+// Postgres numeric type was coming as string
+// But this settings will set it as a number
+types.setTypeParser(pg.types.builtins.NUMERIC, (value: string) => (
+  parseFloat(value)
+))
 
 const connection = knex(config[process.env.NODE_ENV])
 
